366c9ac17a88ba3ddecbe87c91726e8daddf42c6
Rheinard Korf <rheinard@thekorfs.com>
2020-08-05 00:59:43 +1000

    Cleaning up.

--- _11ty/.eleventy.js
+++ _11ty/.eleventy.js
@@ -1,5 +1,14 @@
 module.exports = (eleventyConfig) => {
-    eleventyConfig.addPassthroughCopy({'_11ty/css': 'css'})
+    eleventyConfig.addPassthroughCopy({ '_11ty/css': 'css' })
+
+    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+        if (outputPath.endsWith(".html")) {
+            // Remove the extra [ ] added by Foam.
+            return content.replace(/\[<a/g, '<a').replace(/\/a>]/g,'/a>');
+        }
+
+        return content;
+    });
 
     return {
         dir: {
